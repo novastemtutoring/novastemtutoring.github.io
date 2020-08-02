2dbe97d15dca0044d692510c6f2d418e22eed550
NVST <novastemtutoring@gmail.com>
2020-08-02 14:11:31 -0400

    Updates

---
+++ precache-manifest.6062f8a041b12f55edc07bbd776fa210.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45301dcc6292f6f9a5f9ca84eae86297",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1e7748998313001a129",
+    "url": "/static/css/main.f4353ce3.chunk.css"
+  },
+  {
+    "revision": "5746a0af80113e52ba01",
+    "url": "/static/js/2.9e98b5a7.chunk.js"
+  },
+  {
+    "revision": "5eeaef24d459591afeaec501ac57b571",
+    "url": "/static/js/2.9e98b5a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1e7748998313001a129",
+    "url": "/static/js/main.b360bfda.chunk.js"
+  },
+  {
+    "revision": "732b58cc48af1c41dea0",
+    "url": "/static/js/runtime-main.75446d84.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "b138261a3ec69d6d5b827ce8311f15db",
+    "url": "/static/media/atom.b138261a.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "607b930a36a5c52fd51e0e9e09978eed",
+    "url": "/static/media/heat_engine.607b930a.png"
+  },
+  {
+    "revision": "e4488df7b475812f48eb9d9b996c9722",
+    "url": "/static/media/ideaslab.e4488df7.png"
+  },
+  {
+    "revision": "ff34856724ca13c6b82f38192f5ae2cc",
+    "url": "/static/media/ladyteacher.ff348567.png"
+  },
+  {
+    "revision": "9bdba9d2343a74c3cd6a66e0dcbefda0",
+    "url": "/static/media/linearalgebra.9bdba9d2.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "68ae4690d0cdc98d811ff418c5767c7b",
+    "url": "/static/media/skill.68ae4690.svg"
+  },
+  {
+    "revision": "4a9c2000049e39fcabee0753c0127bbe",
+    "url": "/static/media/uva_centered_logo.4a9c2000.jpg"
+  },
+  {
+    "revision": "d4e0ef368ce1163fcb03dbd19e877bc3",
+    "url": "/static/media/williamandmary.d4e0ef36.png"
+  }
+]);
