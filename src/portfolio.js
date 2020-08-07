958b673a8a34dcd915f543c7a9335229057268e0
NVST <novastemtutoring@gmail.com>
2020-08-07 16:50:02 -0400

    fix spelling

--- src/portfolio.js
+++ src/portfolio.js
@@ -212,7 +212,7 @@ const covid = {
   subTitle: "ALL TUTORING WILL REMAIN VIRTUAL UNTIL FURTHER NOTICE",
   text: "I am very familiar with online learning and presentation platforms! Throughout my graduate studies, " +
     " I worked extensively with scientists all over the world, including people based in Sweden, Japan, and Turkey." +
-    " While Google Meet is my preffered platform (coupled with Jamboard and Google Classroom), I am also familiar with Zoom and Skype. We can certainly find a " +
+    " While Google Meet is my preferred platform (coupled with Jamboard and Google Classroom), I am also familiar with Zoom and Skype. We can certainly find a " +
     "virtual solution in these difficult times!"
 }
 
